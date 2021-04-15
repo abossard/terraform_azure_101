64c3fa3cc22ef5fbd0e9d14dbb4127342dce1ad0
Andre Bossard <anbossar@microsoft.com>
2021-04-15 13:56:05 +0200

    this is fine

--- tf_with_modules/infrastructure/aks_acr.tf
+++ tf_with_modules/infrastructure/aks_acr.tf
@@ -2,19 +2,6 @@ variable "prefix" {
   type = string
 }
 
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.26"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "anbossar-tf-${var.prefix}"
--- tf_with_modules/main.tf
+++ tf_with_modules/main.tf
@@ -1,17 +1,31 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
 module "infrastructure" {
   source = "./infrastructure"
-  prefix = "demo2"
+  for_each = toset(["demo2", "demo3"])
+  prefix = each.key
 }
 
-locals {
-    cluster_name = module.infrastructure.cluster_name
-    registry_name = module.infrastructure.registry_name
-}
+#locals {
+#    cluster_name = module.infrastructure[0].cluster_name
+#    registry_name = module.infrastructure[0].registry_name
+#}
 
 output "cluster_name" {
-    value = local.cluster_name
+    value = module.infrastructure
 }
 
-output "registry_name" {
-    value = local.registry_name
-}
+#output "registry_name" {
+#    value = local.registry_name
+#}
