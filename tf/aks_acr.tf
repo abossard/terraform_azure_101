66729e0cb0840a929004bce2de174fc8a9f84acc
Andre Bossard <andre.bossard@us.abb.com>
2021-06-03 23:29:08 +0200

    updated with new examples

--- tf/aks_acr.tf
+++ tf/aks_acr.tf
@@ -12,29 +12,27 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "anbossar-tf-demo"
-  location = "westeurope"
-}
 
 resource "azurerm_container_registry" "acr" {
   name                     = "anbossartfacs"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg[count.index].name
+  location                 = azurerm_resource_group.rg[count.index].location
   sku                      = "Basic"
   admin_enabled            = false
+  count = 3
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "anbossartfdemo"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = azurerm_resource_group.rg[0].name
   dns_prefix          = "anbossartfdemo"
 
   default_node_pool {
     name       = "default"
     node_count = 1
     vm_size    = "Standard_D2_v2"
+    os_disk_size_gb = 100
   }
 
   identity {
@@ -43,7 +41,14 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 resource "azurerm_role_assignment" "aks_to_acs" {
-  scope                = azurerm_container_registry.acr.id
+  scope                = azurerm_container_registry.acr[count.index].id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  count = 3
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "anbossar-tf-demo-${count.index}"
+  location = "westeurope"
+  count = 3
 }
