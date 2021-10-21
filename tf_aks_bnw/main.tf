7c32dd6fa427b91e6c42379d8080ec035fc32437
Andre Bossard <abossard@users.noreply.github.com>
2021-10-21 10:15:06 +0000

    more is more

--- tf_aks_bnw/main.tf
+++ tf_aks_bnw/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 2.26"
     }
   }
@@ -13,28 +13,25 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host                   = "${azurerm_kubernetes_cluster.aks.kube_admin_config.0.host}"
-  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)}"
-  client_key             = "${base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)}"
-  #load_config_file       = false
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
 }
-
-
 
 provider "helm" {
   debug = true
 
   kubernetes {
-    host                   = "${azurerm_kubernetes_cluster.aks.kube_admin_config.0.host}"
-    client_certificate     = "${base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)}"
-    client_key             = "${base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)}"
-    cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)}"
+    host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
   }
 }
 
 locals {
-  namename = "${var.project-name}${var.postfix}"
+  namename                       = "${var.project-name}${var.postfix}"
   backend_address_pool_name      = "defaultaddresspool"
   frontend_port_name             = "httpPort"
   frontend_ip_configuration_name = "appGatewayFrontendIP"
@@ -43,10 +40,10 @@ locals {
   request_routing_rule_name      = "${local.namename}-rqrt"
 
   aks_resource_group_name = "${local.namename}-mc"
-  virtual_network_name = "${local.namename}-aks-vnet"
-  aks_subnet_name = "${local.namename}-aks-subnet"
-  appgw_subnet_name = "appgwsubnet"
-  
+  virtual_network_name    = "${local.namename}-aks-vnet"
+  aks_subnet_name         = "${local.namename}-aks-subnet"
+  appgw_subnet_name       = "appgwsubnet"
+
   appgw_name = "${local.namename}-appgw"
 
   loga_name = "${local.namename}-loga"
@@ -64,99 +61,99 @@ data "azurerm_resource_group" "node_rg" {
 }
 
 resource "azurerm_virtual_network" "aks_vnet" {
-    name                = local.virtual_network_name
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    address_space       = [var.virtual_network_address_prefix]
+  name                = local.virtual_network_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [var.virtual_network_address_prefix]
 }
 
 resource "azurerm_subnet" "aks" {
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  name           = local.aks_subnet_name
-  address_prefixes = [var.aks_subnet_address_prefix]
+  name                 = local.aks_subnet_name
+  address_prefixes     = [var.aks_subnet_address_prefix]
 }
 
 resource "azurerm_subnet" "appgw" {
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  name           = local.appgw_subnet_name
-  address_prefixes = [var.app_gateway_subnet_address_prefix]
+  name                 = local.appgw_subnet_name
+  address_prefixes     = [var.app_gateway_subnet_address_prefix]
 }
 
 # Public Ip 
 resource "azurerm_public_ip" "public_ip" {
-    name                         = local.public_ip_name
-    location                     = azurerm_resource_group.rg.location
-    resource_group_name          = azurerm_resource_group.rg.name
-    allocation_method            = "Static"
-    sku                          = "Standard"
+  name                = local.public_ip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_application_gateway" "appgw" {
-    name                = local.appgw_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location            = azurerm_resource_group.rg.location
+  name                = local.appgw_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
-    sku {
-      name     = "Standard_v2"
-      tier     = "Standard_v2"
-      capacity = 2
-    }
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
 
-    gateway_ip_configuration {
-      name      = "appGatewayIpConfig"
-      subnet_id = azurerm_subnet.appgw.id
-    }
+  gateway_ip_configuration {
+    name      = "appGatewayIpConfig"
+    subnet_id = azurerm_subnet.appgw.id
+  }
 
-    frontend_port {
-      name = local.frontend_port_name
-      port = 80
-    }
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
 
-    frontend_port {
-      name = "httpsPort"
-      port = 443
-    }
+  frontend_port {
+    name = "httpsPort"
+    port = 443
+  }
 
-    frontend_ip_configuration {
-      name                 = local.frontend_ip_configuration_name
-      public_ip_address_id = azurerm_public_ip.public_ip.id
-    }
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
 
-    backend_address_pool {
-      name = local.backend_address_pool_name
-    }
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
 
-    backend_http_settings {
-      name                  = local.http_setting_name
-      cookie_based_affinity = "Disabled"
-      port                  = 80
-      protocol              = "Http"
-      request_timeout       = 30
-    }
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 30
+  }
 
-    http_listener {
-      name                           = local.listener_name
-      frontend_ip_configuration_name = local.frontend_ip_configuration_name
-      frontend_port_name             = local.frontend_port_name
-      protocol                       = "Http"
-    }
-    request_routing_rule {
-      name = local.request_routing_rule_name
-      http_listener_name = local.listener_name
-      rule_type                  = "Basic"
-      backend_http_settings_name = local.http_setting_name
-      backend_address_pool_name = local.backend_address_pool_name
-    }
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    http_listener_name         = local.listener_name
+    rule_type                  = "Basic"
+    backend_http_settings_name = local.http_setting_name
+    backend_address_pool_name  = local.backend_address_pool_name
+  }
 
-    # request_routing_rule {
-    # name                       = local.request_routing_rule_name
-    # rule_type                  = "Basic"
-    # http_listener_name         = local.listener_name
-    # backend_address_pool_name  = local.backend_address_pool_name
-    # backend_http_settings_name = local.http_setting_name
-    # }
+  # request_routing_rule {
+  # name                       = local.request_routing_rule_name
+  # rule_type                  = "Basic"
+  # http_listener_name         = local.listener_name
+  # backend_address_pool_name  = local.backend_address_pool_name
+  # backend_http_settings_name = local.http_setting_name
+  # }
 }
 # resource "azurerm_user_assigned_identity" "agic_identity" {
 #   resource_group_name = azurerm_resource_group.rg.name
@@ -194,11 +191,11 @@ resource "azurerm_application_gateway" "appgw" {
 # }
 
 resource "azurerm_container_registry" "acr" {
-  name                     = "anbossartfacs"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  sku                      = "Basic"
-  admin_enabled            = false
+  name                = "anbossartfacs"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
 }
 
 
@@ -226,22 +223,20 @@ resource "azurerm_kubernetes_cluster" "aks" {
   role_based_access_control {
     enabled = true
     azure_active_directory {
-      managed = true
-      azure_rbac_enabled = true
+      managed                = true
+      azure_rbac_enabled     = true
       admin_group_object_ids = var.aks_admin_group_objectids
     }
   }
 
   default_node_pool {
-    name       = "default"
-    #node_count = 3
-    enable_auto_scaling = true
-    min_count = 1
-    max_count = 12
-    vm_size    = "Standard_D2_v2"
-    os_disk_size_gb = 100
-    availability_zones = [ "1", "2", "3" ]
-    vnet_subnet_id = azurerm_subnet.aks.id
+    name                         = "system"
+    only_critical_addons_enabled = true
+    node_count                   = 3
+    vm_size                      = "Standard_D2_v2"
+    os_disk_size_gb              = 100
+    availability_zones           = ["1", "2", "3"]
+    vnet_subnet_id               = azurerm_subnet.aks.id
   }
 
   identity {
@@ -250,23 +245,42 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   addon_profile {
     ingress_application_gateway {
-      enabled = true
+      enabled    = true
       gateway_id = azurerm_application_gateway.appgw.id
     }
     oms_agent {
-      enabled = true
+      enabled                    = true
       log_analytics_workspace_id = azurerm_log_analytics_workspace.loga.id
     }
   }
   network_profile {
-    network_plugin = "azure"
+    network_plugin    = "azure"
     load_balancer_sku = "standard"
   }
 
   node_resource_group = local.aks_resource_group_name
 
   # https://docs.microsoft.com/en-us/azure/aks/upgrade-cluster#set-auto-upgrade-channel
-  automatic_channel_upgrade = "patch"  
+  automatic_channel_upgrade = "patch"
+}
+
+
+
+resource "azurerm_kubernetes_cluster_node_pool" "user_pool" {
+  name                  = "workload"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_DS2_v2"
+  mode                  = "User"
+  enable_auto_scaling   = true
+  min_count             = 1
+  max_count             = 12
+  os_disk_size_gb       = 100
+  availability_zones    = ["1", "2", "3"]
+  vnet_subnet_id        = azurerm_subnet.aks.id
+
+  tags = {
+    Environment = "Production"
+  }
 }
 
 resource "azurerm_role_assignment" "aks_to_acs_pull" {
@@ -283,22 +297,22 @@ resource "azurerm_role_assignment" "aks_to_acs_reader" {
 
 
 resource "azurerm_role_assignment" "agentpool_kubelet_msi" {
-  scope = data.azurerm_resource_group.node_rg.id
+  scope                = data.azurerm_resource_group.node_rg.id
   role_definition_name = "Managed Identity Operator"
-  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 
 
 resource "azurerm_role_assignment" "agentpool_kubelet_msi_main" {
-  scope = azurerm_resource_group.rg.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "Managed Identity Operator"
-  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 
 resource "azurerm_role_assignment" "agentpool_vm" {
-  scope = data.azurerm_resource_group.node_rg.id
-  role_definition_name = "Virtual Machine Contributor"
-  principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  scope                            = data.azurerm_resource_group.node_rg.id
+  role_definition_name             = "Virtual Machine Contributor"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   skip_service_principal_aad_check = true
 }
 
@@ -309,9 +323,9 @@ resource "azurerm_role_assignment" "k8s_sa_network_contributor" {
 }
 
 resource "azurerm_role_assignment" "appgw_role_ingress" {
-  scope = azurerm_application_gateway.appgw.id
-  role_definition_name = "Contributor"
-  principal_id = azurerm_kubernetes_cluster.aks.addon_profile[0].ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
+  scope                            = azurerm_application_gateway.appgw.id
+  role_definition_name             = "Contributor"
+  principal_id                     = azurerm_kubernetes_cluster.aks.addon_profile[0].ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
   skip_service_principal_aad_check = true
 }
 
@@ -323,17 +337,24 @@ resource "kubernetes_cluster_role_binding" "aad_integration" {
   }
   role_ref {
     api_group = "rbac.authorization.k8s.io"
-    kind = "ClusterRole"
-    name = "cluster-admin"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
   }
   subject {
-    kind = "Group"
-    name = "system:masters"
+    kind      = "Group"
+    name      = "system:masters"
     api_group = "rbac.authorization.k8s.io"
   }
 }
 
-
+resource "helm_release" "aad-pod-identity" {
+  depends_on = [azurerm_kubernetes_cluster.aks]
+  name       = "aad-pod-identity"
+  repository = "https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts"
+  chart      = "aad-pod-identity"
+  version    = "4.1.6"
+  namespace  = "kube-system"
+}
 
 # # Allows all get list of namespaces, otherwise tools like 'kubens' won't work
 # resource "kubernetes_cluster_role" "all_can_list_namespaces" {
--- tf_aks_bnw/variables.tf
+++ tf_aks_bnw/variables.tf
@@ -1,54 +1,54 @@
 variable "project-name" {
   description = "Funny name this all belongs to"
-  default = "megusta"
+  default     = "megusta"
 }
 
 variable "postfix" {
   description = "Randon stuff to add everywhere"
-  default = "-an12"
+  default     = "-an12"
 }
 
 variable "kubernetes_version" {
   description = "Version of Kubernetes to install"
-  default = "1.20.9"
+  default     = "1.20.9"
 }
 
 variable "aks_admin_group_objectids" {
-    description = "Object IDs of groups to be added to the Aks Admin Role"
-    default = ["d095b256-5ac9-480d-923a-2c6ca0825ffe"]
+  description = "Object IDs of groups to be added to the Aks Admin Role"
+  default     = ["d095b256-5ac9-480d-923a-2c6ca0825ffe"]
 }
 
 variable "location" {
-    description = "Location of the cluster."
-    default = "westeurope"
+  description = "Location of the cluster."
+  default     = "westeurope"
 }
 
 variable "virtual_network_address_prefix" {
-    description = "VNET address prefix"
-    default     = "15.0.0.0/8"
+  description = "VNET address prefix"
+  default     = "15.0.0.0/8"
 }
 
 variable "aks_subnet_address_prefix" {
-    description = "Subnet address prefix."
-    default     = "15.0.0.0/16"
+  description = "Subnet address prefix."
+  default     = "15.0.0.0/16"
 }
 
 variable "app_gateway_subnet_address_prefix" {
-    description = "Subnet server IP address."
-    default     = "15.1.0.0/16"
+  description = "Subnet server IP address."
+  default     = "15.1.0.0/16"
 }
 
 variable "aks_name" {
-    description = "AKS cluster name"
-    default     = "aks-cluster1"
+  description = "AKS cluster name"
+  default     = "aks-cluster1"
 }
 
 variable "aks_service_cidr" {
-    description = "CIDR notation IP range from which to assign service cluster IPs"
-    default     = "10.0.0.0/16"
+  description = "CIDR notation IP range from which to assign service cluster IPs"
+  default     = "10.0.0.0/16"
 }
 
 variable "aks_dns_service_ip" {
-    description = "DNS server IP address"
-    default     = "10.0.0.10"
+  description = "DNS server IP address"
+  default     = "10.0.0.10"
 }
